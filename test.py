cd7e286f322dc143fe8dfa06160c730a890771d6
Jordan <jordan.tran2@ucalgary.ca>
2026-08-19 11:44:50 -0600

    introduction to ThreadPoolExecutor

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 # this file is for testing the functionality of certain features before they are appended to the main script file
 from pathlib import Path # turns a directory into an object you can iterate through
+from concurrent.futures import ThreadPoolExecutor # allows for the use of threads, so you can call functions non-sequentially
 import hashlib # allows you to hash files, nothing major
 import psutil # mandatory for checking for open processes
+import time # for simple timer functionality
+
+save_path = Path("C:/Users/shado/Downloads")
 
 def is_running(program_name):
     for process in psutil.process_iter(['name']):
@@ -11,10 +15,29 @@ def is_running(program_name):
             return True
     return False
 
-save_path = Path("C:/Users/shado/Downloads")
+def monitor_process(program_name):
+    while True:
+        # waiting for any emulator to open
+        while True:
+            if is_running(program_name):
+                print(program_name)
+                break
 
-if is_running("pcsx2-qt.exe"):
-    print("PCSX2 is running!")
-else:
-    print("PCSX2 is not running.")
+            time.sleep(5) #just waits 5 seconds before checking again
+
+        # waiting for the emulator to close
+        while True:
+            if not is_running(program_name):
+                print(program_name)
+                break
+
+            time.sleep(5) # effectively the same code, but to check if it has closed
+
+
+emulators = ["Dolphin.exe", "pcsx2-qt.exe"]
+
+with ThreadPoolExecutor() as executor:
+    executor.map(monitor_process, emulators) # use threading to run the function simultaneously on all elements
+
+
 

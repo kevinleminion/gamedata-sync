dc9bb4228786a9c0c36b426bfeaa6fce5ada537a
Jordan <jordan.tran2@ucalgary.ca>
2026-08-19 22:54:00 -0600

    configuring program to interact with and write to Azure

--- test.py
+++ test.py
@@ -4,8 +4,17 @@ from concurrent.futures import ThreadPoolExecutor # allows for the use of thread
 import hashlib # allows you to hash files, nothing major
 import psutil # mandatory for checking for open processes
 import time # for simple timer functionality
+from threading import Lock # so only one thread can write code at a time
+from azure.storage.fileshare import ShareClient # allow writing to an Azure File Share
 
 save_path = Path("C:/Users/shado/Downloads")
+manifest_lock = Lock() # create a lock object
+
+def update_manifest(emulator, new_hash): # only allows one thread in this block at once
+    with manifest_lock:
+        manifest = read_manifest_file()
+        manifest[emulator] = new_hash
+        write_manifest_file(manifest)
 
 def is_running(program_name):
     for process in psutil.process_iter(['name']):
@@ -20,7 +29,7 @@ def monitor_process(program_name):
         # waiting for any emulator to open
         while True:
             if is_running(program_name):
-                print(program_name)
+                print(program_name + " is running.")
                 break
 
             time.sleep(5) #just waits 5 seconds before checking again
@@ -28,16 +37,34 @@ def monitor_process(program_name):
         # waiting for the emulator to close
         while True:
             if not is_running(program_name):
-                print(program_name)
+                print(program_name + " was closed.")
                 break
 
             time.sleep(5) # effectively the same code, but to check if it has closed
 
+connection_string = ""
+share_name = "storeddata"  # whatever you named it
+
+share = ShareClient.from_connection_string(connection_string, share_name) # connect to azure
+
+directory = share.get_directory_client("")  # "" means the root of the share
+for item in directory.list_directories_and_files():
+    print(item) # print out everything in the directory (should be empty for now)
+
+file_client = share.get_file_client("test.txt") # what the file is called remotely 
+
+with open("data.txt", "rb") as source_file:
+    file_client.upload_file(source_file) # write this to the remote file with the same name
+
+print("upload complete")
+share.close()
+
 
 emulators = ["Dolphin.exe", "pcsx2-qt.exe"]
 
-with ThreadPoolExecutor() as executor:
-    executor.map(monitor_process, emulators) # use threading to run the function simultaneously on all elements
+# with ThreadPoolExecutor() as executor:
+#     for emulator in emulators:
+#         executor.submit(monitor_process, emulator)
 
 
 

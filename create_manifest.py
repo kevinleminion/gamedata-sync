a8dac5f48354503e5134e62354482cf31e7f1f0f
Jordan <jordan.tran2@ucalgary.ca>
2026-09-01 12:13:03 -0600

    script to create manifest is working and functional, may require a bit of tuning

--- create_manifest.py
+++ create_manifest.py
@@ -21,7 +21,7 @@ def iterate_through_local(file_path):
             with open(file, "rb") as file_to_hash:
                 file_data = file_to_hash.read()
     
-            relative_path = file.relative_to(directory) # get the relative path
+            relative_path = str(file.relative_to(directory)) # get the relative path, NEEDS to be a string
             timestamp = os.path.getmtime(file) # timestamp the file
             file_hash = hashlib.sha256(file_data).hexdigest() # hash the file, then turn it into a string
 
@@ -29,8 +29,6 @@ def iterate_through_local(file_path):
             return_list.append(entry)
 
     return return_list
-
-
 
 config_data = parse_config_file("config.json") # grabbing the needed data
 emulator_data = config_data["emulators"]
@@ -40,4 +38,8 @@ manifest_dictionary = {"data_entries": []} # initial dictionary to create the ma
 for emulator_name, emulator_values in emulator_data.items():
     for entry in emulator_values["local_save_path"]:
         manifest_dictionary["data_entries"].extend(iterate_through_local(entry["local"])) # extend to add each individual list item 
+
+with open("manifest.json", "w") as file:
+    json.dump(manifest_dictionary, file, indent = 4) # turn the dictionary into a json
+    # indent = 4 makes it easier to read
 
